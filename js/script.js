9f4be85adb4a2251397b7aa0bf1f1a32b6fbcd01
abdallah abo zaid <abdallahabozaid1999@gmail.com>
2021-08-07 23:21:53 +0300

    portfolio page for hebsma education

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    document.querySelector(".loader ").classList.toggle("hide")
+}
+
+document.querySelector(".navbar-toggler").onclick = function () {
+    document.querySelector(".navbar-toggler span.sec-p").classList.toggle("open")
+}
+
